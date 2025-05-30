f2aeba982666a1e6d0c23581319bfa16256e04ba
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-05-30 12:33:38 +0200

    Add allowedHosts option for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(() => {
+  const allowedHosts = process.env.__VITE_ADDITIONAL_SERVER_ALLOWED_HOSTS
+    ?.split(",")
+    .map((h) => h.trim()) || ["localhost"];
+
+  return {
+    plugins: [vue()],
+    server: {
+      allowedHosts,
+    },
+  };
 });
