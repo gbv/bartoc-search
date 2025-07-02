809b6ee22449a1cd2d45b980c7ca6ecf3c086001
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-07-02 15:17:08 +0200

    Display solr status info in footer (#21)

--- src/client/components/TheFooter.vue
+++ src/client/components/TheFooter.vue
@@ -2,12 +2,47 @@
   <footer class="footer">
     <div class="footer__container">
       <div class="footer__info">
-        <span>&copy; {{ new Date().getFullYear() }} Bartoc Search</span>
+        <span>&copy; {{ year }} Bartoc Search</span>
         <span class="footer__links">
           <a href="https://github.com/gbv/bartoc-search" target="_blank" rel="noopener">GitHub</a>
           <a href="/about">About</a>
         </span>
       </div>
+      <div>
+        <div> Solr Status : {{ Object.entries(solrStatus)
+            .map(([key, value]) => `${key} = ${value}`)
+            .join(' ---- ') }}
+        </div>
+      </div>
     </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+import { reactive, onMounted } from 'vue'
+import axios from 'axios'
+import { SolrStatusResult } from '../../server/types/solr'
+
+const year = new Date().getFullYear()
+
+const solrStatus = reactive<SolrStatusResult>({
+  connected: false,
+  indexedRecords: 0,
+  lastIndexedAt: '',
+  lastUpdate: '',
+  firstUpdate: '',
+})
+
+onMounted(async () => {
+  try {
+    const res = await axios.get(`${import.meta.env.BASE_URL}status`)
+    Object.assign(solrStatus, res.data.solr)
+  } catch (e) {
+    console.warn('Could not fetch Solr status')
+  }
+})
+
+function formatJson(val: any) {
+  return val ? JSON.stringify(val, null, 2) : ''
+}
+</script>
