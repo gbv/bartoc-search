e805b79265843aeb6b319c285ae767d1db9bfdd5
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-07-02 14:24:12 +0200

    Minimal style/structure header footer hero (#16)

--- src/client/views/HomeView.vue
+++ src/client/views/HomeView.vue
@@ -3,7 +3,15 @@ import SearchBar  from '../components/SearchBar.vue'
 </script>
 
 <template>
-  <main>
-     <SearchBar />
+  <main class="hero__wrapper">
+    <section class="hero">
+      <h1 class="hero__title">
+        BARTOC Search <small class="hero__subtitle">prototype for testing</small>
+      </h1>
+      <div class="hero__search">
+        <SearchBar />
+      </div>
+    </section>
   </main>
 </template>
+
