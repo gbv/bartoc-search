9a9c5982c3bd6dd3d81bc0d0e7d5a886a58613a2
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-07-31 09:06:08 +0200

    Change labels (#37)

--- src/client/constants/facetFieldLabels.js
+++ src/client/constants/facetFieldLabels.js
@@ -9,14 +9,17 @@ import SUPPORTED_LANGUAGES from "../../../data/supported_languages.json"
  * Maps the internal facet field names to display titles.
  */
 export const FACET_FIELD_LABELS = {
+  type_uri: {
+    label: "Knowledge Organization System Type",
+    values: KOS_TYPE_LABELS,
+  },
+  /* ddc_ss: {
+    label: "Dewey Decimal Classification",
+    values: {},
+  }, */
   languages_ss:  {
     label: "Language",
     values: SUPPORTED_LANGUAGES,
-  },
-  // add more facet fields here as needed
-  type_uri: {
-    label: "Kos Type",
-    values: KOS_TYPE_LABELS,
   },
   publisher_label: {
     label: "Publisher",
