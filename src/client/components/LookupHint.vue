4f8908673620a29d00657db967f61770f696865b
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-08-13 14:14:10 +0200

    Few adjustments in LookupHint component (#54)

--- src/client/components/LookupHint.vue
+++ src/client/components/LookupHint.vue
@@ -24,20 +24,20 @@ const resolvedUri = computed(() => {
   <div
     v-if="hasData"
     class="lookup-message">
-    <div class="lookup-message__uri">
+    <span class="lookup-message__uri">
       <a
         :href="uri"
         target="_blank">{{ uri }}</a>
-    </div>
-    <div>is likely an URI from</div>
-    <div class="lookup-message__name">
+    </span>
+    <span>is likely an URI from</span>
+    <span class="lookup-message__name">
       <a
         :href="resolvedUri || '#'"
         target="_blank"
         :class="{ disabled: !resolvedUri }">
         {{ name }}
       </a>
-    </div>
+    </span>
   </div>
 </template>
 
@@ -46,12 +46,15 @@ const resolvedUri = computed(() => {
   color: var(--black);
   display: flex;
   justify-content: center;
+  padding: 12px;
 }
 .lookup-message__uri {
   background: #ff9;
+  margin-right: 5px;
 }
 .lookup-message__name {
   font-weight: bold;
+  margin-left: 5px;
 }
 a.disabled {
   pointer-events: none;
