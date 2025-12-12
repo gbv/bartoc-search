65c81575bee7eaa045bf617aa8befe95e676ddd5
Jakob Voss <voss@gbv.de>
2025-12-12 09:36:21 +0100

    Remove HomeView and move base URL from /search to /
    The only purpose of this application is to provide the discovery
    interface, to be hosted at <https://bartoc.org/search/>, so there is no
    need to add a start page as <https://bartoc.org/> is the start page.

--- src/client/router/routes.js
+++ src/client/router/routes.js
@@ -1,16 +1,14 @@
-import HomeView from "../views/HomeView.vue"
 import SearchView from "../views/SearchView.vue"
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: SearchView,
   },
   {
     path: "/search",
-    name: "search",
-    component: SearchView,
+    redirect: "/",
   },
 ]
 
