a3b7d03641e22c1bd3bfcfe651677fef46f206f3
Jakob Voss <voss@gbv.de>
2025-12-12 11:08:45 +0100

    Adjust footer (#98)

--- src/client/components/TheFooter.vue
+++ src/client/components/TheFooter.vue
@@ -1,20 +1,31 @@
 <template>
   <footer>
-    <div v-if="apiStatus.solr.connected">
-      search in <b>{{ apiStatus.solr.indexedRecords }}</b> terminologies
-      (as of <b>{{ apiStatus.solr.lastIndexedAt }}</b>, live updates <b>{{ apiStatus.jskosServer.connected ? 'enabled' : 'disabled' }}</b>)
-    </div>
-    <div v-else>
-      Search index not available!
-    </div>
     <div>
-      <a
-        :href="`${baseUrl}api/status`"
-        target="_blank">API</a>
+      <span v-if="apiStatus.solr.connected">
+        search in <b>{{ apiStatus.solr.indexedRecords }}</b> terminologies
+        (as of <b>{{ apiStatus.solr.lastIndexedAt }}</b>,
+        live updates <b>{{ apiStatus.jskosServer.connected ? 'enabled' : 'disabled' }}</b>)
+      </span>
+      <span v-else>
+        Search index not available!
+      </span>
+      <a :href="`${baseUrl}api/status`">Search API</a>
+      |
+      <a href="https://github.com/gbv/bartoc-search">sources</a>
+    </div>
+    <p>
+      BARTOC.org vocabulary metadata is <a href="/download">made available</a> under the <a href="http://www.opendatacommons.org/licenses/pddl/1.0/">PDDL 1.0</a>
+    </p>
+    <div>
+      <a href="/api/">JSKOS API</a>
       |
       <a
-        href="https://github.com/gbv/bartoc-search"
-        target="_blank">GitHub</a>
+        rel="me"
+        href="https://code4lib.social/@bartoc">Mastodon</a>
+      |        
+      <a href="https://github.com/gbv/bartoc.org">sources</a>
+      |
+      <a href="https://github.com/gbv/bartoc.org/issues">issues</a>
     </div>
   </footer>
 </template>
