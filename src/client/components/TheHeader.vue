d806289ed3a07cf97fba30840eddfbc2774ebe80
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2026-01-02 11:16:09 +0100

    Insert links to header, aling to bartoc.org (#98)

--- src/client/components/TheHeader.vue
+++ src/client/components/TheHeader.vue
@@ -8,6 +8,21 @@
             alt="BARTOC Search"
             class="header__logo-image">
         </a>
+        <a href="/vocabularies">
+          <span class="header__logo-text">Vocabularies</span>
+        </a>
+        <a href="/registries">
+          <span class="header__logo-text">Registries</span>
+        </a>
+        <a href="/software">
+          <span class="header__logo-text">Software</span>
+        </a>
+        <a href="/about">
+          <span class="header__logo-text">About</span>
+        </a>
+        <a href="/contact">
+          <span class="header__logo-text">Contact</span>
+        </a>
       </div>
     </div>
   </header>
