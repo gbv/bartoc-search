b0abc63a78c3908af1180d4ca829b2e0012d2a37
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2026-01-02 12:11:11 +0100

    Insert all links without login (#98)

--- src/client/components/TheHeader.vue
+++ src/client/components/TheHeader.vue
@@ -8,22 +8,31 @@
             alt="BARTOC Search"
             class="header__logo-image">
         </a>
-        <a href="/vocabularies">
-          <span class="header__logo-text">Vocabularies</span>
-        </a>
-        <a href="/registries">
-          <span class="header__logo-text">Registries</span>
-        </a>
-        <a href="/software">
-          <span class="header__logo-text">Software</span>
-        </a>
-        <a href="/about">
-          <span class="header__logo-text">About</span>
-        </a>
-        <a href="/contact">
-          <span class="header__logo-text">Contact</span>
-        </a>
       </div>
+      <nav class="header__nav">
+        <div class="header__nav-left">
+          <a href="/vocabularies">
+            <span class="header__logo-text">Vocabularies</span>
+          </a>
+          <a href="/registries">
+            <span class="header__logo-text">Registries</span>
+          </a>
+          <a href="/software">
+            <span class="header__logo-text">Software</span>
+          </a>
+          <a href="/about">
+            <span class="header__logo-text">About</span>
+          </a>
+          <a href="/contact">
+            <span class="header__logo-text">Contact</span>
+          </a>
+        </div>
+        <div class="header__nav-right">
+          <a href="/stats">
+            <span class="header__logo-text">Statistics</span>
+          </a>
+        </div>
+      </nav>
     </div>
   </header>
 </template>
